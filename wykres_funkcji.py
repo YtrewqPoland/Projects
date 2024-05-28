485f3571a872c0c4a80f8b61763289f8aa5aba95
YtrewqPoland <simidawid@gmail.com>
2024-05-28 10:19:55 +0200

    finished project

--- wykres_funkcji.py
+++ wykres_funkcji.py
@@ -10,26 +10,29 @@ def plot_function():
     y2 = []
     a = int(input("Podaj początek dziedziny funkcji: "))
     b = int(input("Podaj koniec dziedziny funkcji (większe niż poprzednia!): "))
-    if '/x' in function_formula or "/ x" in function_formula:
-        for x in range(a, -1):
-            x1.append(x)
-            y1.append(eval(function_formula))
-        for x in range(1, b):
-            x2.append(x)
-            y2.append(eval(function_formula))
+    if a >= b:
+        print("Nieprawidłowa dziedzina funkcji.")
     else:
-        for x in range(a, b):
-            x1.append(x)
-            y1.append(eval(function_formula))
-    
-    # Rysowanie wykresu
-    plt.plot(x1, y1, color = "blue")
-    plt.plot(x2, y2, color = "blue")
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Wykres funkcji: y = ' + function_formula)
-    plt.grid(True)
-    plt.show()
+        if '/x' in function_formula or "/ x" in function_formula:
+            for x in range(a, -1):
+                x1.append(x)
+                y1.append(eval(function_formula))
+            for x in range(1, b):
+                x2.append(x)
+                y2.append(eval(function_formula))
+        else:
+            for x in range(a, b):
+                x1.append(x)
+                y1.append(eval(function_formula))
+        
+        # Rysowanie wykresu
+        plt.plot(x1, y1, color = "blue")
+        plt.plot(x2, y2, color = "blue")
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Wykres funkcji: y = ' + function_formula)
+        plt.grid(True)
+        plt.show()
 try:
     plot_function()
 except ZeroDivisionError:
