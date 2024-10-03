978e9369008b9190fec6008c479202c5856c9fd7
YtrewqPoland <simidawid@gmail.com>
2024-10-03 20:33:37 +0200

    started GUI successfully

--- source.py
+++ source.py
@@ -1,5 +1,5 @@
 #import bibliotek
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import tkinter as tk
@@ -14,68 +14,37 @@ def wykres_app():
     okno.destroy()
 
 #pobieranie danych do wykresu
-global count
-count = 1
-def wart_wykres():
-     global count
-     count = pol_tekst.get()
-     pol_tekst2 = tk.Entry(okno_1, width=30)
-     pol_tekst2.grid(row=2, column=0)
-     return count
+def wart_wykres_formula():
+     global function_formula
+     function_formula = pol_tekst.get()
+     global a
+     a = pol_tekst1.get()
+     global b
+     b = pol_tekst2.get()
+     a = int(a)
+     b = int(b)
+     plot_function(function_formula, a, b)
+     return function_formula
 
-def plot_function(count, compartments, function_formula):
-    if count > 0 and count < 5:
-        for i in range(count):
-            if i == 0:
-                 colorc = "blue"
-            if i == 1:
-                 colorc = "black"
-            if i == 2:
-                 colorc = "red"
-            if i == 3:
-                 colorc = "green"
-            if i == 4:
-                 colorc = "yellow"
-            compartments = int(input("Z ilu przedziałów składa się funkcja?: "))
-            for j in range(compartments):
-                if j > 5:
-                    print("Zbyt duża liczba przedziałów!")
-                # Pobieranie wzoru funkcji od użytkownika
-                function_formula = input("Podaj wzór funkcji (pamiętaj o znakach składni Pythona!): ")
-                x1 = []
-                x2 = []
-                y1 = []
-                y2 = []
-                a = int(input("Podaj początek dziedziny funkcji: "))
-                b = int(input("Podaj koniec dziedziny funkcji (większe niż poprzednia!): "))
-                c = a
-                d = b
-                if a >= b:
-                    print("Nieprawidłowa dziedzina funkcji.")
-                else:
-                    if '/x' in function_formula or "/ x" in function_formula:
-                            x = np.arange(a, 0, 0.01)
-                            y = eval(function_formula)
-                            plt.plot(x, y, color = colorc)
-                            x = np.arange(0.01, b + 0.01, 0.01)
-                            y = eval(function_formula)
-                            plt.plot(x, y, color = colorc)
-                    else:
-                            x = np.arange(a, b + 0.01, 0.01)
-                            y = eval(function_formula)
-                            plt.plot(x, y, color = colorc)
-            x = c
-            plt.scatter(x, eval(function_formula), s = 50, facecolor = colorc)
-            x = d
-            plt.scatter(x, eval(function_formula), s = 50, facecolor = colorc)
+def plot_function(function_formula, a, b):
+            if '/x' in function_formula or "/ x" in function_formula:
+                    x = np.arange(a, 0, 0.01)
+                    y = eval(function_formula)
+                    plt.plot(x, y, color = "red")
+                    x = np.arange(0.01, b + 0.01, 0.01)
+                    y = eval(function_formula)
+                    plt.plot(x, y, color = "red")
+            else:
+                    x = np.arange(a, b + 0.01, 0.01)
+                    y = eval(function_formula)
+                    plt.plot(x, y, color = "red")
             plt.xlabel('x')
             plt.ylabel('y')
             plt.grid(True)
             plt.axhline(y=0, color='black', linestyle='solid')
             plt.axvline(x=0, color='black', linestyle='solid')
-        plt.show()
-    else:
-         print("Błąd!")
+            plt.show()
+
 
 # Tworzenie głównego okna
 okno = tk.Tk()
@@ -93,15 +62,23 @@ okno.mainloop()
 if app == 1:
     okno_1 = tk.Tk()
     okno_1.title("Rysowanie wykresu funkcji")
-    okno_1.geometry("400x300+100+100")
-
+    okno_1.geometry("800x300+100+100")
     pol_tekst = tk.Entry(okno_1, width=30)
-    pol_tekst.grid(row=1, column=0)
-    input_button1=tk.Button(okno_1, text="Potwierdź", command=wart_wykres)
-    input_button1.grid(row = 1, column = 1)
-    print(count)
-    plot_function(count, 0, 0)
+    pol_tekst.grid(row=1, column=1)
+    info = tk.Label(okno_1, text="Podaj wzór funkcji (pamiętaj o znakach składni Pythona!): ")
+    info.grid(row=1, column=0)
+    pol_tekst1 = tk.Entry(okno_1, width=30)
+    pol_tekst1.grid(row=2, column=1)
+    info1 = tk.Label(okno_1, text="Podaj początek dziedziny funkcji: ")
+    info1.grid(row=2, column=0)
+    pol_tekst2 = tk.Entry(okno_1, width=30)
+    pol_tekst2.grid(row=3, column=1)
+    info2 = tk.Label(okno_1, text="Podaj koniec dziedziny funkcji: ")
+    info2.grid(row=3, column=0)
+    input_button1=tk.Button(okno_1, text="Potwierdź", command=wart_wykres_formula)
+    input_button1.grid(row = 4, column = 0, columnspan=2)
     okno_1.mainloop()
+    
     
     
     
